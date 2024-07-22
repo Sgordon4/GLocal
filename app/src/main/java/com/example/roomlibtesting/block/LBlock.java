6e88f391bbc64e5590fe37c583c8c25f46df5143
SGordon4 <seangordonkh@gmail.com>
2024-07-22 18:07:37 -0500

    Adding tables

---
+++ app/src/main/java/com/example/roomlibtesting/block/LBlock.java
@@ -0,0 +1,26 @@
+package com.example.roomlibtesting.block;
+
+import androidx.annotation.NonNull;
+import androidx.room.ColumnInfo;
+import androidx.room.Entity;
+import androidx.room.PrimaryKey;
+
+@Entity(tableName = "block")
+public class LBlock {
+	@PrimaryKey
+	@NonNull
+	public String blockhash;
+
+	@ColumnInfo(defaultValue = "0")
+	public int blocksize;
+
+	@ColumnInfo(defaultValue = "CURRENT_TIMESTAMP")
+	public long createtime;
+
+
+	public LBlock(@NonNull String blockhash) {
+		this.blockhash = blockhash;
+		this.blocksize = 0;
+		this.createtime = -1;
+	}
+}
---
+++ app/src/main/java/com/example/roomlibtesting/journal/LJournal.java
@@ -0,0 +1,55 @@
+package com.example.roomlibtesting.journal;
+
+import androidx.annotation.NonNull;
+import androidx.room.ColumnInfo;
+import androidx.room.Entity;
+import androidx.room.ForeignKey;
+import androidx.room.PrimaryKey;
+
+import com.example.roomlibtesting.account.LAccount;
+import com.example.roomlibtesting.file.LFile;
+
+import java.util.Date;
+import java.util.UUID;
+
+@Entity(tableName = "journal",
+		foreignKeys = {
+			@ForeignKey(entity = LAccount.class,
+			parentColumns = "accountuid",
+			childColumns = "accountuid",
+			onDelete = ForeignKey.CASCADE),
+			@ForeignKey(entity = LFile.class,
+			parentColumns = "fileuid",
+			childColumns = "fileuid",
+			onDelete = ForeignKey.CASCADE)
+		})
+public class LJournal {
+	@PrimaryKey(autoGenerate = true)
+	public long journalid;
+
+	@NonNull
+	public UUID fileuid;
+	@NonNull
+	public UUID accountuid;
+
+	@ColumnInfo(defaultValue = "0")
+	public int filesize;
+
+	@NonNull
+	@ColumnInfo(defaultValue = "[]")
+	public String fileblocks;
+
+	@ColumnInfo(defaultValue = "CURRENT_TIMESTAMP")
+	public long changetime;
+
+
+
+	public LJournal(@NonNull UUID fileuid, @NonNull UUID accountuid) {
+		this.fileuid = fileuid;
+		this.accountuid = accountuid;
+
+		this.filesize = 0;
+		this.fileblocks = "[]";
+		this.changetime = -1;
+	}
+}
